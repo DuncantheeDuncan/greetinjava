4265ba4142b5f24f75f1541335a17d9d676d2955
DuncantheeDuncan <mthembup69@gmail.com>
2020-05-01 03:00:59 +0200

    pulling

--- hide/target/classes/db/migration/V1__Create_person_table.sql
+++
@@ -1,8 +0,0 @@
---drop table if exists countperson cascade;
-
-create table PERSON(
-    id INT PRIMARY KEY AUTO_INCREMENT NOT NUll,
-    name text not null,
-    counter int not null,
-
-);
--- hide/target/classes/db/migration/V2__add_person.sql
+++
@@ -1,3 +0,0 @@
-
-
---insert into PERSON (name, counter) values ('phumlani', 1);
